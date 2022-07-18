2d72938abf3eae6886049c346bd15ede343570c5
SeonYeong An <asy100@naver.com>
2022-07-18 14:00:19 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+fun main(){
+
+    var sc = Scanner(System.`in`)
+
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
