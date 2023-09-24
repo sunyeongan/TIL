19e6f708d6e760c6ed82e6ceb28900bc5bcf8925
SeonYeong An <asy100@naver.com>
2023-09-24 15:12:14 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.37 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Solution {
+public int solution(int price) {
+int answer = 0;
+
+    if(price>=100000 && price<300000){
+        answer=(price-(int)(Math.Ceiling(price*0.05)) );
+        
+    }else if(price>=300000 && price<500000){
+        answer=(price-(int)(Math.Ceiling(price*0.1)));
+    }else if(price>=500000 && price<=1000000){
+        answer=(price-(int)(Math.Ceiling(price*0.2)));
+    }else {
+        answer=price;
+    }
+    return answer;
+}
+}
