6e43a186a63099b3d93378880b4e0769667208e8
SeonYeong An <asy100@naver.com>
2024-11-23 15:59:07 -0500

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+import sys
+
+score = int(sys.stdin.readline())
+
+if 90<= score <=100:
+    print("A")
+elif 80<= score <90:
+    print("B")
+elif 70<= score <80:
+    print("C")
+elif 60<= score <70:
+    print("D")
+else:
+    print("F")
